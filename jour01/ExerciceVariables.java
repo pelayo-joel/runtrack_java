2680f9d0833a268bf685af4f00636b610752f430
pelayo-joel <joel.pelayo@laplateforme.io>
2023-05-09 18:39:05 +0200

    [FIX] Fixed a bug in 'ExerciceVariables.java'

--- jour01/ExerciceVariables.java
+++ jour01/ExerciceVariables.java
@@ -4,7 +4,9 @@ public class ExerciceVariables {
         System.out.println("La valeur de num1 est " + num1 + " et la valeur de num2 est " + num2);
 
         //Swap using bitwise operators (exclusive or operators)
-        num1 ^= num2 ^= num1;
+        num1 ^= num2; 
+        num2 ^= num1;
+        num1 ^= num2;
         System.out.println("La valeur de num1 est " + num1 + " et la valeur de num2 est " + num2);
     }
 }
